fdf217ab36e7ad91668aa80b546ee017812b4629
Markus Koppenberger <koppi@action.at>
2016-05-10 21:33:25 +0200

    changed structure to work with PlatformIO and using now .h and .cpp files

---
+++ src/Remote/src/main.cpp
@@ -0,0 +1,19 @@
+/*
+ NIMAN SUB ROV - Remote control
+
+ NimanRov is a project to build a Remotely Operated underwater Vehicle.
+ This is the code which will run on the arduino board in the ROV itself.
+
+ */
+
+#include "Arduino.h"
+#include <SPI.h>
+#include <Ethernet.h>
+
+void setup() {
+
+}
+
+void loop() {
+
+}
